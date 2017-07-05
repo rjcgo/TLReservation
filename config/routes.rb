875209d22f2f3c0c1092faf6449bf9f72aa42c86
Rj Go <rcgo3@up.edu.ph>
2017-07-05 13:42:16 +0800

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+
+	match ':controller(/:action)', :via => :get
+
+	root :to => 'home#index'
+	get 'home/index'
+
+	# resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
