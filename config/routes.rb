44f3fd7fec4250285c84ba6c79f6390b911c5395
Daniel <dlhizon@up.edu.ph>
2017-07-06 09:31:55 +0000

    route ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  
-  resources :reservations
-  resources :testlines
-	devise_for :users, controllers: { registrations: "registrations" }
+
+	devise_for :users
+
+	devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+	end
+	
 	match ':controller(/:action)', :via => :get
 	
 	root :to => 'home#index'
 	get 'home/index'
   get '/index', to: 'home#index'
 	get '/schedule', to: 'home#schedule'
-
+	
+	resources :reservations
+  resources :testlines
 end
