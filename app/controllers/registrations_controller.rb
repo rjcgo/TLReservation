19bf889a92d36eaeaddaa753c423fdd4f5d59874
Rj Go <rcgo3@up.edu.ph>
2017-07-07 11:37:38 +0800

    account type fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
     private
     
     def sign_up_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:account_type, :name, :email, :password, :password_confirmation)
     end
     def account_update_params
-       params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password) 
+       params.require(:user).permit(:account_type, :name, :email, :password, :password_confirmation, :current_password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	devise_for :users
+	devise_for :users, :controllers => {registrations:  'registrations'}
 
 	devise_scope :user do  
    get '/users/sign_out' => 'devise/sessions#destroy'  
