35e0b578475cb3071ba6bd98ad09f84cf97aa824
Daniel <dlhizon@up.edu.ph>
2017-07-10 07:53:27 +0000

    Medj okay

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710041749) do
+ActiveRecord::Schema.define(version: 20170710072404) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170710041749) do
     t.integer  "testline_id"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "team_name"
+    t.text     "description"
   end
 
   add_index "reservations", ["testline_id"], name: "index_reservations_on_testline_id"
