71596f04e75ef3f9eb95b8b7a8c2930863671e3d
Rj Go <rcgo3@up.edu.ph>
2017-07-12 16:11:40 +0800

    Team scaffold

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#burger-nav").on("click", function(event){
 
         $("#burger-nav").toggleClass("change");
-        $(".sidebar-nav").toggleClass("open");
+        $(".navbar-nav").toggleClass("open");
     });
 
 });
