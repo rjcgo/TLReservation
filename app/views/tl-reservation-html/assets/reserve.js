eef961487273148402a8b37842e6eabb4cab2de1
Yves <yvesfrancisco95@gmail.com>
2017-07-12 10:22:43 -0800

    added reserve.html and made adjustment to sizing in home.html

---
+++ app/views/tl-reservation-html/assets/reserve.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#burger-nav").on("click", function(event){
+
+        $("#burger-nav").toggleClass("change");
+        $(".navbar-nav").toggleClass("open");
+    });
+
+    $(".sidenav-toggle").on("click", function(event){
+        event.preventDefault();
+        $(".sidenav").toggleClass("toggle");
+    });
+
+    $(".closebtn").on("click", function(event){
+        event.preventDefault();
+        $(".sidenav").removeClass("toggle");
+    })
+});
