3b051766c99dceb941cac4ef2843c62a3602738f
Princess Danielle Florendo <pvflorendo@up.edu.ph>
2017-07-12 18:07:54 +0800

    Added isAdmin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712083240) do
+ActiveRecord::Schema.define(version: 20170712100641) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170712083240) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "isAdmin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
