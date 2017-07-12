8ae1ecd081c5868051f0ccc07abe4f9521d3e271
Princess Danielle Florendo <pvflorendo@up.edu.ph>
2017-07-12 18:04:18 +0800

    Log in added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :teams
   resources :testlines do
     resources :reservations
