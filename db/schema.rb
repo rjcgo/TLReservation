10c2936c9c10a90e2c7578139aa3af378c23f2bf
Jennie Ron S. Ablog <jennieablog@gmail.com>
2017-07-13 13:18:02 +0800

    Fixed isadmin if statement

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170713022837) do
+ActiveRecord::Schema.define(version: 20170713031045) do
 
 # Could not dump table "reservations" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
