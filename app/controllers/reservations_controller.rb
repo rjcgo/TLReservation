53bd1dd18172369720513b47d0e7bfebf8132444
Jennie Ron S. Ablog <jennieablog@gmail.com>
2017-07-13 16:07:47 +0800

    Minor edits to redirects and home page view.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,13 +3,13 @@ class ReservationsController < ApplicationController
     def create
         @testline = Testline.find(params[:testline_id])
         @reservation = @testline.reservations.create(params[:reservation].permit(:name, :released, :team_name, :description, :email))
-        redirect_to testlines_path
+        redirect_to root_path
     end
     def destroy
         @testline = Testline.find(params[:testline_id])
         @reservation = @testline.reservations.find(params[:id])
         @reservation.destroy
-        redirect_to testline_path(@testline)
+        redirect_to root_path
     end
     def edit
     
