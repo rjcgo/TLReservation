fe4153fdc2a2f686eea9e46426049137bbf1e667
digitalsirkeith <kvargas@up.edu.ph>
2017-07-15 11:52:35 +0800

    added team dropdown functionality

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,28 @@
 module TeamsHelper
+	def available
+		@mytestlines.each do |testline|
+			if testline.reservations.count == 0
+				return true
+			end
+		end
+		return false
+	end
+
+	def inUse
+		@mytestlines.each do |testline|
+			if testline.reservations.count != 0
+				return true
+			end
+		end
+		return false
+	end
+
+	def maintenance
+		@mytestlines.each do |testline|
+			if testline.isMaintenance?
+				return true
+			end
+		end
+		return false
+	end
 end
--- app/helpers/testlines_helper.rb
+++ app/helpers/testlines_helper.rb
@@ -1,31 +1,4 @@
 module TestlinesHelper
-	def available
-		@testlines.each do |testline|
-			if testline.reservations.count == 0
-				return true
-			end
-		end
-		return false
-	end
-
-	def inUse		
-		@testlines.each do |testline|
-			if testline.reservations.count != 0
-				return true
-			end
-		end
-		return false
-	end
-
-	def maintenance
-		@testlines.each do |testline|
-			if testline.isMaintenance?
-				return true
-			end
-		end
-		return false
-	end
-
 	def firstReserve
 		@testlines.each do |testline|
 			if testline.reservations.count != 0
