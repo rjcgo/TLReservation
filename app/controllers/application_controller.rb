3c64b07b8c9f91a23b465a3ef97cf0b2def0967d
digitalsirkeith <kvargas@up.edu.ph>
2017-07-18 13:11:55 +0800

    added modals for delete and cancel reservation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
-    render :text => exception, :status => 500
-  end
-  protect_from_forgery with: :exception
-  
+	rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+		render :text => exception, :status => 500
+	end
+	protect_from_forgery with: :exception
+	
+	def after_sign_in_path_for(resource)
+  		testlines_reservations_path
+	end
+
+	def after_sign_out_path_for(resource_or_scope)
+		root_path
+	end
 end
