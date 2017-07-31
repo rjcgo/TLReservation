111d8af0f23752ddeb4a042091cd9df1ba969f6d
Princess Danielle Florendo <pvflorendo@up.edu.ph>
2017-07-31 10:59:02 +0800

    updated application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,20 +5,22 @@ module ApplicationHelper
         content_tag(:u1, "") do
 	        @reservations.each do |reserve| 
 	            if testline.id == reserve.testline_id 
-	                tag += content_tag(:li, "") do
-	                	tag1 = content_tag(:p, reserve.team_name, :class => "card-text")
-						if currentReserve == 0
-							tag1 += content_tag(:small, "Current", :class => "badge badge-primary") 
-						elsif currentReserve == 1 
-							tag1 += content_tag(:small, "Next in Line", :class => "badge badge-next")
-						else 
-							tag1 += content_tag(:small, "Pending", :class => "badge badge-next")
-						end 
-
-						tag1 + content_tag(:button, "", :class => "delete-overlay show-delete-modal") do
-							content_tag(:i, "", :class => "fa fa-calendar-check-o") + " #{reserve.email}"
-	                	end
-	                end
+	            	if currentReserve < 4
+		                tag += content_tag(:li, "") do
+		                	tag1 = content_tag(:p, reserve.team_name, :class => "card-text")
+							if currentReserve == 0
+								tag1 += content_tag(:small, "Current", :class => "badge badge-primary") 
+							elsif currentReserve == 1 
+								tag1 += content_tag(:small, "Next in Line", :class => "badge badge-next")
+							elsif currentReserve == 2 || currentReserve == 3
+								tag1 += content_tag(:small, "Pending", :class => "badge badge-next")
+							end 
+								
+							tag1 + content_tag(:button, "", :class => "delete-overlay show-delete-modal") do
+								content_tag(:i, "", :class => "fa fa-calendar-check-o") + " #{reserve.email}"
+		                	end
+		                end
+		            end
 					currentReserve += 1
 	            end 
 	        end 
